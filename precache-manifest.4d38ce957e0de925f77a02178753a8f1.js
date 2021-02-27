68552f1a7c399320753388b6a67617a3b0886930
Purvesh Thakkar <thakkarpurvesh1997@gmail.com>
2021-02-27 13:58:39 +0530

    Updates

---
+++ precache-manifest.4d38ce957e0de925f77a02178753a8f1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99f2f3bc6690b61b967f7ea2bc56e048",
+    "url": "/Covid-19/index.html"
+  },
+  {
+    "revision": "819a81e813b64cf5863a",
+    "url": "/Covid-19/static/css/2.c746ff85.chunk.css"
+  },
+  {
+    "revision": "16b9c46f07ad6172ff4d",
+    "url": "/Covid-19/static/css/main.6a389dc5.chunk.css"
+  },
+  {
+    "revision": "819a81e813b64cf5863a",
+    "url": "/Covid-19/static/js/2.1f268ad7.chunk.js"
+  },
+  {
+    "revision": "f1882e7d89cef6386fde43da35464e77",
+    "url": "/Covid-19/static/js/2.1f268ad7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16b9c46f07ad6172ff4d",
+    "url": "/Covid-19/static/js/main.feb31743.chunk.js"
+  },
+  {
+    "revision": "c34b4e2701dd19dd7097",
+    "url": "/Covid-19/static/js/runtime-main.3f70e205.js"
+  },
+  {
+    "revision": "ce7126949bf962bc8c52a7f6755078fe",
+    "url": "/Covid-19/static/media/call.ce712694.svg"
+  },
+  {
+    "revision": "567b3b9b122339706bbdfe725d8a685f",
+    "url": "/Covid-19/static/media/distance.567b3b9b.svg"
+  },
+  {
+    "revision": "8cad16d7cf74240c309f3d4e31db7f73",
+    "url": "/Covid-19/static/media/doctor.8cad16d7.svg"
+  },
+  {
+    "revision": "e669dac1dc6606f900fe0529fa116559",
+    "url": "/Covid-19/static/media/food.e669dac1.svg"
+  },
+  {
+    "revision": "1062b7c02cf6cbd1b7b9a193c8164f0b",
+    "url": "/Covid-19/static/media/home.1062b7c0.svg"
+  },
+  {
+    "revision": "287f4bcfe2337a0c1f5b3d241794b20c",
+    "url": "/Covid-19/static/media/wash-hand.287f4bcf.svg"
+  },
+  {
+    "revision": "5de63bcea7098ecfd281ea18d9bab873",
+    "url": "/Covid-19/static/media/workout.5de63bce.svg"
+  }
+]);
